da332e2a6f9a0b2cffc73162375d0d1492f9346b
Didi <adihorowitz@campus.technion.ac.il>
2019-05-06 14:01:10 +0300

    my_script

--- hw2/models.py
+++ hw2/models.py
@@ -192,7 +192,7 @@ class YourCodeNet(ConvClassifier):
             layers.append(nn.Linear(in_size, out_size))
             layers.append(nn.BatchNorm1d(out_size))
             layers.append(nn.ReLU())
-            layers.append(nn.Dropout2d(p=0.2))
+        layers.append(nn.Dropout2d(p=0.2))
         layers.append(nn.Linear(hidden_dimensions[-1], self.out_classes))
         seq = nn.Sequential(*layers)
         return seq
