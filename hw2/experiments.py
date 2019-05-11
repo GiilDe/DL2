890a697bd12594087aa91a84ef457df5d201fae8
gilad212387 <38991777+gilad212387@users.noreply.github.com>
2019-05-11 16:02:50 +0300

    radio bla bla

--- hw2/experiments.py
+++ hw2/experiments.py
@@ -56,7 +56,10 @@ def run_experiment(run_name, out_dir='./results', seed=None,
     #  for you automatically.
     fit_res = None
     x_in, y_out = ds_train[0]
-    model = model_cls(in_size=x_in.shape, out_classes=10, filters=filters_per_layer*layers_per_block, pool_every=pool_every,
+
+    filters = [f for f in filters_per_layer for i in range(layers_per_block)]
+
+    model = model_cls(in_size=x_in.shape, out_classes=10, filters=filters, pool_every=pool_every,
                    hidden_dims=hidden_dims, **kw).to(device)
     loss = torch.nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr,weight_decay=reg)
